5d58bc9b38cdd46cbc2313078b06db61c9bbbd14
Dion Snoeijen <hallo@dionsnoeijen.nl>
2016-09-11 19:38:13 +0200

    Initial commit

---
+++ docker/api/app/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const ampq = require('amqplib');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello world!\n');
+});
+
+app.listen(PORT);
+
+console.log('Running on http://localhost:' + PORT);
